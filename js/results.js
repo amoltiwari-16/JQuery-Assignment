bfb914920f1f916414f50d3192912d91ca1078b9
Amol Tiwari <amoltiwari.16@gmail.com>
2016-11-02 10:01:11 +0000

    Issues fixed

--- js/results.js
+++ js/results.js
@@ -1,11 +1,14 @@
 $(function()
-{
-	var $results = $('#results');
+{	
+	var results = $('.movies');
+	//search button onclick code
 	$('#srchbtn').on('click',function()
 	{	
-		$('#results').empty();
+		$('.movies').empty();
 		$('#results').css("background","#eee").css("height","auto");
+		$('body').css("background","#eee");
 		$('#pagin').empty();
+		$('.res-header').empty();
 		var movieName=$('#movie').val();
 		var foundMatch= false; 
 		var pgCount=0;
@@ -14,11 +17,19 @@ $(function()
 			type:'GET',
 			url: 'http://www.omdbapi.com/?s='+movieName,
 			success :function(movies)
-			{	$results.append('<h2 class="text-center">MOVIES</h2><br>')
-				$.each(movies["Search"], function(i,movie){ 
+			{	$('.res-header').append('<h3>Results.........</h3><br>')
+					if(movies.Response=="False")
+				{	
+					$('#results').css("background","#eee").css("height","250px");
+					$('.res-header').css("background","#eee");
+					$('.res-header').empty();
+					results.append('<h3 class="text-center">No match found, kindly search with some other keyword</h3>');
+				}else
+				{
+					$.each(movies["Search"], function(i,movie){ 
 						foundMatch=true;
 						pgCount++;
-						$results.append('<div class="line-content text-center"><img src="'+movie.Poster+'" alt="Pic" width="180" height="180"><br><strong>Title : '+movie.Title+'</strong><br>Year : '+movie.Year+'<br>ImdbID : '+movie.imdbID+'<br>Type : '+movie.Type+'<br></div>');
+						results.append('<div class="line-content list-group-item text-center"><img src="'+movie.Poster+'" alt="Pic" width="180" height="180"><br><strong>Title : '+movie.Title+'</strong><br>Year : '+movie.Year+'<br>ImdbID : '+movie.imdbID+'<br>Type : '+movie.Type+'<br></div>');
 				});
 
 				//pagination
@@ -40,21 +51,26 @@ $(function()
 					        }
 			    	});        
 				}
+				//Initial page display
 				showPage(1);
 				$('#pagin li a').click(function() {
 				    $('#pagin li a').removeClass("current");
 				    $(this).addClass('current');
 				    showPage(parseInt($(this).text())) 
 				});
-		
-				if(foundMatch==false)
-				{	
-					$('#results').css("background","#eee").css("height","250px");
-					$results.append('<h3 class="text-center">No match found, kindly search with some other keyword</h3>');
+
 				}
+				
+				// Check if no record is present
+				// if(!foundMatch)
+				// {	
+				// 	$('#results').css("background","#eee").css("height","250px");
+				// 	results.append('<h3 class="text-center">No match found, kindly search with some other keyword</h3>');
+				// }
 			},
+			//error message display if error occurs
 			error : function(){
-				$results.append('Sorry, unable to fetch data');
+				results.append('Sorry, unable to fetch data');
 			}
 		});
 	})
